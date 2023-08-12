ffeb50e6074f55d4619e1bbe360eb42293b1b117
juniecodes <activatejunie>
2023-08-11 21:33:16 -0500

    completed assignment

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import styles from "./styling.css";
+
+function Banner() {
+    return (
+        <div>
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
+
+export default Banner;
